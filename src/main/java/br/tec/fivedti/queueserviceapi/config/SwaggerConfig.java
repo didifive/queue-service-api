f2fa09f2c446585cfbcb8fb7de88de9be6991362
Luis Carlos <luisczdidi@gmail.com>
2022-04-16 08:50:50 -0300

    Update SwaggerConfig

--- src/main/java/br/tec/fivedti/queueserviceapi/config/SwaggerConfig.java
+++ src/main/java/br/tec/fivedti/queueserviceapi/config/SwaggerConfig.java
@@ -4,23 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     private static final String BASE_PACKAGE = "br.tec.fivedti.queueserviceapi.controllers";
     private static final String API_TITLE = "Queue Service API";
     private static final String API_DESCRIPTION = "REST API for Queue Service";
-    private static final String CONTACT_NAME = "Luis Carlos";
+    private static final String CONTACT_NAME = "Luis Zancanela";
     private static final String CONTACT_GITHUB = "https://gtihub.com/didifive";
     private static final String CONTACT_EMAIL = "luisczdidi@gmail.com";
 
