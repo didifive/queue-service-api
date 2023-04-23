09495c115c9cd219adaa758507e659c88fe8c0c3
Luis Zancanela <luisczdidi@gmail.com>
2023-04-23 17:58:09 -0300

    feat: update with redirect root to swagger

--- src/main/java/br/tec/didiproject/queueserviceapi/controllers/MainController.java
+++ src/main/java/br/tec/didiproject/queueserviceapi/controllers/MainController.java
@@ -3,13 +3,12 @@ package br.tec.didiproject.queueserviceapi.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class MainController {
     @GetMapping
-    public ModelAndView redirectToSwagger() {
-        return new ModelAndView("redirect:/swagger-ui.html");
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
     }
 }
